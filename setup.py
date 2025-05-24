b540537b07e8248f0b01aae632c22c1433559339
jhillenbrand <hillenbrandjonas@hotmail.com>
2025-05-24 12:34:53 +0200

    corrected setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="PyDataGrabber",
     version="0.0.1",
-    packages=find_packages("src", exclude=["tests"]),
-    package_dir = {"": "src"}
+    packages=find_packages(exclude=["tests"])
 )
