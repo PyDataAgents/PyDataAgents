551ff3a58f990f5113df1f8a00f9d0209633bea4
jhillenbrand <hillenbrandjonas@hotmail.com>
2025-05-31 09:09:49 +0200

    added project toml

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
  
 setup(
     name="PyDataGrabber",
-    version="0.0.1",
-    packages=find_packages(exclude=["tests", "app"])
+    version="0.1",
+    packages=find_packages(exclude=["tests"])
 )
