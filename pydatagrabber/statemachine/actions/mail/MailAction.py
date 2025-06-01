88add5b45709d28c1c46763617934675c2f319f7
jhillenbrand <hillenbrandjonas@hotmail.com>
2025-06-01 11:31:28 +0200

    added mail action

---
+++ pydatagrabber/statemachine/actions/mail/MailAction.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass, field
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+from ...StatemachineException import StatemachineException
+from ...Action import Action
+
+
+@dataclass
+class MailAction(Action):
+    
+    smtp_server : str = field(default=None, metadata={"description": "host of the mail server to use"})
+    port : int = field(default=None, metadata={"description": "port of the smtp server"})
+    mail_account : str = field(default=None, metadata={"description": "mail account to use for login"})
+    pw : str = field(default=None, metadata={"description": "password of the mail server"})
+    recipient : str = field(default=None, metadata={"description": "mail address of the recipient"})
+    subject : str = field(default=None,  metadata={"description": "subject of the mail"})
+    body : str = field(default=None,  metadata={"description": "body of the mail"})
+    
+    def __init__(self):
+        super().__init__()
+        
+    def execute(self):
+        # Create message
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Test Email from Python via GMX"
+        message["From"] = self.mail_account
+        message["To"] = self.recipient
+        
+        mt = MIMEText(self.body, "html")
+        
+        message.attach(mt)
+        
+        # Send
+        try:
+            with smtplib.SMTP(self.smtp_server, self.port) as server:
+                server.starttls()  # Secure the connection
+                server.login(self.mail_account, self.pw)
+                server.sendmail(self.mail_account, self.recipient, message.as_string())
+                #print("✅ Email sent successfully.")
+        except Exception as e:
+            raise StatemachineException("could not send mail from " + self.cname()) from e
---
+++ tests/actions/mail/test_mail_action.py
@@ -0,0 +1,19 @@
+import configparser
+from pydatagrabber.statemachine.actions.mail.MailAction import MailAction
+
+
+def test_000():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    ma = MailAction()
+    ma.smtp_server = config["GMX"]["smtp_server"]
+    ma.mail_account = config["GMX"]["watchdog_mail"]
+    ma.port = int(config["GMX"]["smtp_port"])
+    ma.pw = config["GMX"]["watchdog_pw"]
+    ma.sender = ma.mail_account
+    ma.recipient = config["GMX"]["test_mail"]
+    ma.subject = "Test Mail"
+    ma.body = "<h1>Header</h1><p>That's a paragraph</p>"
+    
+    ma.execute()
+       
