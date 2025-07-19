180cb5a4a9b899b1d567c00299447a9fea76745b
jhillenbrand <hillenbrandjonas@hotmail.com>
2025-07-19 11:36:32 +0200

    changed default of mapping

--- pydg/mappings/Mapping.py
+++ pydg/mappings/Mapping.py
@@ -9,9 +9,9 @@ class Mapping(GrabberElement):
     """
     """
     
-    buffer_ids : list[str] = field(default_factory=list, metadata={"description": "list of buffer ids to map from"})
+    buffer_ids : list[str] = field(default_factory=None, metadata={"description": "list of buffer ids to map from"})
     adapter_id : str = field(default=None, metadata={"description": "id of the Adapter used for this Mapping"})
-    addresses : list[str] = field(default_factory=list, metadata={"description": "list of addresses to read/subscribe from or write/publish to"})
+    addresses : list[str] = field(default_factory=None, metadata={"description": "list of addresses to read/subscribe from or write/publish to"})
     thread_type : str = field(default=ThreadType.MILLI_SECOND.value, metadata={"description": "type of thread, e.g. MILLI_SECONDS, MICRO_SECONDS, INSTANT, ONLY_ONCE, ..."})
     mapping_type : str = field(default=None, metadata={"description": "type of mapping, e.g. READ, WRITE, SUB or PUB"})
     n : int = field(default=1, metadata={"description": "number of samples to insert or remove from buffers"})
