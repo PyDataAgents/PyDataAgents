faad6fd156aa863975e2bd063bb22edd3b34ed09
jhillenbrand <hillenbrandjonas@hotmail.com>
2025-07-31 01:53:13 +0200

    refactored folder dataelements

---
+++ pydg/statemachine/actions/DataElementAction.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass, field
+
+from ...buffers.DictBuffer import DictBuffer
+from ...grabbers.GrabberElement import GrabberElement
+from ...grabbers.Grabber import Grabber
+from ..Action import Action
+from ..BufferNode import BufferNode
+
+
+@dataclass
+class DataElementAction(BufferNode, Action):
+    
+    n : int = field(default=0, metadata={"description": "Number of samples to retrieve from the parents' buffers."})
+    persistent : bool = field(default=False, metadata={"description": "if true, data will persist in parents' buffers after retrieval"})
+    
+    MAX_DEFAULT_CAPACITY = 1e6
+        
+    def __init__(self):
+        super().__init__()
+        
+    
+    def install(self, grabber : Grabber):
+        GrabberElement.install(grabber)
+        if self.buffer is None:
+            if self.buffer_id in grabber.buffer_store:
+                self.buffer = grabber.buffer_store[self.buffer_id]
+            else:
+                self.buffer = DictBuffer()
+                self.buffer.id = self.id + "-BUFFER"
+                self.buffer.capacity = DataElementAction.MAX_DEFAULT_CAPACITY
+    
