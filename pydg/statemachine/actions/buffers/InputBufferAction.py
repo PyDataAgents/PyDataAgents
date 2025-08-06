2a95f086bbd6fadc69119ab3ba833d89651da460
jhillenbrand <hillenbrandjonas@hotmail.com>
2025-08-06 07:29:35 +0200

    changed inputbuffer action

--- pydg/statemachine/actions/buffers/InputBufferAction.py
+++ pydg/statemachine/actions/buffers/InputBufferAction.py
@@ -5,10 +5,7 @@ from .DataElementAction import DataElementAction
 class InputBufferAction(DataElementAction):
     """
     `DataElementAction` for retrieving data from a `Grabber` `Buffer` to a `DataElementAction`.
-    """
-        
-    def __init__(self):
-        super().__init__()        
+    """      
     
     def execute(self):
         # nothing to do here, as the DataElementAction is just a link to an existing buffer
