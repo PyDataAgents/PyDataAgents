80f51a0f69120325789fd9a95a19f44e4f14ae3b
2Obe <74399383+2Obe@users.noreply.github.com>
2025-08-19 22:13:12 +0200

    Convert to list in cases np.arrays() are written to the buffer..

--- pydag/buffers/DictBuffer.py
+++ pydag/buffers/DictBuffer.py
@@ -5,6 +5,7 @@ from typing import Union
 
 from .Buffer import Buffer
 from ..agents import Agent
+import numpy as np
 
 class DictBuffer(Buffer):
     """buffer that stores its values in a dictionary in a table like fashion, where every key contains a list of data
@@ -31,6 +32,8 @@ class DictBuffer(Buffer):
                     if k in self.elements.keys():
                         if isinstance(elements[k], list):
                             self.elements[k].extend(elements[k])
+                        elif isinstance(elements[k], np.ndarray):
+                            self.elements[k].extend(elements[k].tolist())
                         else:
                             self.elements[k].append(elements[k])
                         # check for infinity capacity
