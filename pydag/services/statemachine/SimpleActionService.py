959c48adee3d2cf89c08d11b7abf064631f06cfe
jhillenbrand <hillenbrandjonas@hotmail.com>
2025-09-28 22:28:53 +0200

    implemented SimpleActionService

--- pydag/services/statemachine/SimpleActionService.py
+++ pydag/services/statemachine/SimpleActionService.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass, field
+from __future__ import annotations
 
+from loguru import logger
+
+from ...nodes.NodeException import NodeException
 from ...agents.Agent import Agent
 from ...mappings.Observer import Observer
 from ...mappings.ObserverThread import ObserverThread
@@ -9,8 +13,32 @@ from ...services.Service import Service
 
 
 class SimpleActionObserver(Observer):
-    pass
+    
+    def __init__(self, service: SimpleActionService):
+        super().__init__()
+        self.service = service
 
+    def observe(self):
+        for node in self.service.nodes:
+            if isinstance(node, Action):
+                node.activate()
+                if self.service.retry_error_nodes:
+                    try:
+                        node.execute()
+                    except NodeException as e:
+                        logger.error(f"Reattempting Node {node.id} execution: {e}")
+                        node.execute()
+                else:
+                    node.execute()
+                node.deactivate()
+    
+    def unobserve(self):
+        """
+        Unobserve the statemachine.
+        This method is called when the statemachine is being stopped
+        """
+        if self.service.is_running:
+            self.service.stop()
 
 @dataclass
 class SimpleActionService(Service):
@@ -28,3 +56,12 @@ class SimpleActionService(Service):
         super().install(agent)
         for node in self.nodes:
             node.install(agent)
+            
+    def start(self):
+        self.observer_thread = ObserverThread(id=ObserverThread.unique_id(), thread_type=ThreadType[self.thread_type])
+        observer = SimpleActionObserver(self)
+        self.observer_thread.add_observer(observer)
+        self.observer_thread.start()
+
+    def stop(self):
+        self.is_running = False
