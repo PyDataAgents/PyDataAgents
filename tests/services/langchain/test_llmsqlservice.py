e5d63fcb36899df88fcf3a8891f29463511cf8b7
jhillenbrand <hillenbrandjonas@hotmail.com>
2025-10-13 07:47:14 +0200

    debugging llmsqlservice

--- tests/services/langchain/test_llmsqlservice.py
+++ tests/services/langchain/test_llmsqlservice.py
@@ -2,13 +2,16 @@ import configparser
 import os
 from langchain_community.utilities import SQLDatabase
 
+from pydag.agents.Agent import Agent
+from pydag.services.langchain.LLMRestService import LLMRestService
 from pydag.services.langchain.LLMSQLService import LLMSQLService
 
 
 def test_000():
-    db = SQLDatabase.from_uri("sqlite:///" + "/" + os.getcwd().replace("\\", "/") + "/tests/data/db/Chinook.db")
+    db = SQLDatabase.from_uri("sqlite:///" + os.path.dirname(__file__).replace("\\", "/") + "/Chinook.db")
     print(db.dialect)
     print(db.get_usable_table_names())
+    print(db.get_context())
     print(db.run("SELECT * FROM Artist LIMIT 10;"))
     
 def test_001():
@@ -20,7 +23,7 @@ def test_001():
     lss.api_key = config["OPENAI"]["OPENAI_API_KEY"]
     lss.model = "gpt-4o"
     lss.model_provider = "OPENAI"
-    lss.sql_connection = "sqlite:///" + os.getcwd().replace("\\", "/") + "/tests/data/db/Chinook.db"
+    lss.sql_connection = "sqlite:///" + os.path.dirname(__file__).replace("\\", "/") + "/Chinook.db"
         
     lss.install()
     lss.start()
@@ -35,7 +38,7 @@ def test_002():
     lss.api_key = config["OPENAI"]["OPENAI_API_KEY"]
     lss.model = "gpt-4o"
     lss.model_provider = "OPENAI"
-    lss.sql_connection = "sqlite:///" + os.getcwd().replace("\\", "/") + "/tests/data/db/Chinook.db"
+    lss.sql_connection = "sqlite:///" + os.path.dirname(__file__).replace("\\", "/") + "/Chinook.db"
     lss.install()
     lss.start()
     print(lss._write_query({"question": "What is the most popular artist based on invoices of customers?"}))
@@ -49,7 +52,7 @@ def test_010():
     lss.api_key = config["OPENAI"]["OPENAI_API_KEY"]
     lss.model = "gpt-4o"
     lss.model_provider = "OPENAI"
-    lss.sql_connection = "sqlite:///" + os.getcwd().replace("\\", "/") + "/tests/data/db/Chinook.db"
+    lss.sql_connection = "sqlite:///" + os.path.dirname(__file__).replace("\\", "/") + "/Chinook.db"
     
     lss.install()
     
@@ -66,7 +69,7 @@ def test_011():
     lss.endpoint = "http://localhost:11434"
     lss.model = "sqlcoder:latest"
     lss.model_provider = "OLLAMA"
-    lss.sql_connection = "sqlite:///" + os.getcwd().replace("\\", "/") + "/tests/data/db/Chinook.db"
+    lss.sql_connection = "sqlite:///" + os.path.dirname(__file__).replace("\\", "/") + "/Chinook.db"
     
     lss.install()
     
@@ -76,3 +79,23 @@ def test_011():
     print(ans)
     
     
+def test_020():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    
+    ag = Agent()
+    
+    lss = LLMSQLService()
+    lss.id = "S1"
+    lss.api_key = config["OPENAI"]["OPENAI_API_KEY"]
+    lss.model = "gpt-4o"
+    lss.model_provider = "OPENAI"
+    lss.sql_connection = "sqlite:///" + os.path.dirname(__file__).replace("\\", "/") + "/Chinook.db"
+    
+    ag.add_service(lss)
+    
+    lrs = LLMRestService(id="S2", port=10004)
+    
+    ag.add_service(lrs)
+    
+    ag.start_blocking()
