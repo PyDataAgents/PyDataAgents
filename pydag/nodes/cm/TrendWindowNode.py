f098b1b6479a0952ebfb3c08cdf4a38890d78d62
jhillenbrand <hillenbrandjonas@hotmail.com>
2026-03-12 07:53:46 +0100

    corrected name of trendwindow

--- pydag/nodes/cm/TrendWindowNode.py
+++ pydag/nodes/cm/TrendWindowNode.py
@@ -5,7 +5,7 @@ from pydag.nodes.BufferNode import BufferNode
 
 
 @dataclass
-class TrendCollectionNode(BufferNode, Action):
+class TrendWindowNode(BufferNode, Action):
     """ This `BufferNode` collects windowed data of the linked parents of specified size `n` with the current timestamp as key prefix to the original key.
     Whenever it is executed it generates a new window to keep (up to `max_windows`). If the maximum number of windows are reached, it discards the one closest to any other window, based on timestamp.
 
