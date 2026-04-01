901e6f9b45d87af28b594fcbf3495eabcbb87075
jhillenbrand <hillenbrandjonas@hotmail.com>
2026-04-01 22:07:19 +0200

    implemented api key based security for restservice

--- pydag/services/rest/RESTAPIManager.py
+++ pydag/services/rest/RESTAPIManager.py
@@ -1,13 +1,18 @@
+from __future__ import annotations
 from enum import Enum
 import os
-from typing import Optional
+from typing import TYPE_CHECKING
 import secrets
-from bs4 import BeautifulSoup
-from fastapi import Depends, HTTPException, Header
 import webbrowser
+from bs4 import BeautifulSoup
+from fastapi import Depends, HTTPException, Security
+from fastapi.security import APIKeyHeader
 
 
 from ...utils.HTMLUtils import HTMLUtils
+
+if TYPE_CHECKING:    
+    from .RestService import RestService
 
 
 class APIRole(str, Enum):
@@ -18,9 +23,9 @@ class APIRole(str, Enum):
     
 class RESTAPIManager:
     
-    _api_keys : dict = {}  # Store API keys and their associated roles
+    API_KEYS : dict = {}  # Store API keys and their associated roles
 
-    _role_hierarchies = {
+    ROLE_HIERARCHY = {
         APIRole.ADMIN: 3,
         APIRole.WRITE: 2,
         APIRole.READ: 1
@@ -28,48 +33,57 @@ class RESTAPIManager:
     
     API_KEY_TEMPLATE = os.path.dirname(__file__) + os.sep + "API_KEY_TEMPLATE.html"
     
+    REST_SERVICE : RestService = None
+    
     @staticmethod
-    def generate_api_keys(api_key_file : str = None):
+    def generate_api_keys(api_key_file : str = None, service : RestService = None):
         """Generate a API keys for this application and roles."""        
-        soup : BeautifulSoup = HTMLUtils.open_html_doc(RESTAPIManager.API_KEY_TEMPLATE)
-        for role, hierarchy in RESTAPIManager._role_hierarchies.items():
-            key = secrets.token_hex(16)
-            if role not in RESTAPIManager._api_keys:
-                RESTAPIManager._api_keys[role] = key
-                HTMLUtils.replace_value_by_id(soup, f"apiKey-{role.lower()}", key)
-        if api_key_file:
-            HTMLUtils.save_html_doc(soup, api_key_file)
-            abs_path = os.path.abspath(api_key_file)
+        RESTAPIManager.REST_SERVICE = service  # Set the REST_SERVICE reference for API key checks
+        # check if api_key_file already exists and load existing keys if so
+        if api_key_file and os.path.exists(api_key_file):
+            soup = HTMLUtils.open_html_doc(api_key_file)
+            for role in APIRole:
+                element = soup.find(id=f"apiKey-{role.value.lower()}")
+                if element and element.has_attr('value'):
+                    RESTAPIManager.API_KEYS[role] = element['value']
         else:
-            HTMLUtils.save_html_doc(soup, "API_KEYS.html")
-            abs_path = os.path.abspath("API_KEYS.html")
+            soup : BeautifulSoup = HTMLUtils.open_html_doc(RESTAPIManager.API_KEY_TEMPLATE)
+            for role, hierarchy in RESTAPIManager.ROLE_HIERARCHY.items():
+                key = secrets.token_hex(16)
+                if role not in RESTAPIManager.API_KEYS:
+                    RESTAPIManager.API_KEYS[role] = key
+                    HTMLUtils.replace_value_by_id(soup, f"apiKey-{role.lower()}", key)
+            if api_key_file:
+                HTMLUtils.save_html_doc(soup, api_key_file)
+            else:
+                api_key_file = "API_KEYS.html"
+                HTMLUtils.save_html_doc(soup, api_key_file)        
+        abs_path = os.path.abspath(api_key_file)
         webbrowser.open(f"file://{abs_path}")
         
 
     @staticmethod
-    def get_api_key(authorization : Optional[str] = Header(default=None)) -> str:
+    def get_api_key(api_key: str = Security(APIKeyHeader(name="Authorization", auto_error=False))) -> str:
         """Get the role associated with the provided API key."""
-        if not authorization:
-           raise HTTPException(status_code=401, detail="Missing Authorization header")        
-        try:
-            scheme, key = authorization.split(" ")
-        except ValueError as exc:
-            raise HTTPException(status_code=401, detail="Invalid Authorization format") from exc
-        if scheme != "ApiKey":
-            raise HTTPException(status_code=401, detail="Invalid auth scheme")
-        return key
+        if not RESTAPIManager.REST_SERVICE:
+            return None  # If API keys are not required, return None
+        if not api_key:
+           raise HTTPException(status_code=401, detail="Missing Authorization header")                
+        return api_key
 
     @staticmethod
     def get_role(api_key: str = Depends(get_api_key)) -> APIRole:
-        for role, keys in RESTAPIManager._api_keys.items():
-            if api_key in keys:
+        if not RESTAPIManager.REST_SERVICE:
+            return APIRole.ADMIN  # If API keys are not required, treat all requests as ADMIN
+        for role, key in RESTAPIManager.API_KEYS.items():
+            if api_key == key:
                 return APIRole(role)
         raise HTTPException(status_code=403, detail="Invalid API key")
     
     @staticmethod   
     def require_min_role(min_role : APIRole):
         def checker(role: APIRole = Depends(RESTAPIManager.get_role)):
-            if RESTAPIManager._role_hierarchies[role] < RESTAPIManager._role_hierarchies[min_role]:
+            if RESTAPIManager.ROLE_HIERARCHY[role] < RESTAPIManager.ROLE_HIERARCHY[min_role]:
                 raise HTTPException(status_code=403, detail="Insufficient permissions")
             return role
         return checker
--- tests/unit/services/rest/test_restapi_manager.py
+++ tests/unit/services/rest/test_restapi_manager.py
@@ -1,10 +1,44 @@
 import os
+import time
 
+from pydag.agents.Agent import Agent
 from pydag.services.rest.RESTAPIManager import RESTAPIManager
 from pydag.utils.FileUtils import FileUtils
+from pydag.services.rest.RestService import RestService
 
 
 def test_api_key_generation():
     api_key_file = os.path.dirname(__file__) + os.sep + "test_API_KEYS.html"
     RESTAPIManager.generate_api_keys(api_key_file=api_key_file)    
     assert FileUtils.exists_file(api_key_file), "test_API_KEYS.html should be generated"
+    
+def test_rest_service_with_api_keys():
+    
+    ag = Agent()
+    
+    service = RestService(port=8002, api_key_file=os.path.dirname(__file__) + os.sep + "test_API_KEYS.html")
+    
+    ag.add_service(service)
+        
+    ag.release(blocking=False)
+    
+    time.sleep(3)
+    
+    ag.terminate()
+    
+    
+def test_rest_service_with_no_api_keys():
+    
+    ag = Agent()
+    
+    service = RestService(port=8002)
+    
+    ag.add_service(service)
+        
+    ag.release(blocking=False)
+    
+    time.sleep(3)
+    
+    ag.terminate()
+    
+    
