eb55233a449518c612e868e2874d6069388202bd
jhillenbrand <hillenbrandjonas@hotmail.com>
2026-04-03 14:57:50 +0200

    implementing and testing sql action

--- pydag/nodes/db/SQLAction.py
+++ pydag/nodes/db/SQLAction.py
@@ -1,30 +1,62 @@
 from dataclasses import dataclass, field
+import pyodbc
+from sqlalchemy import table, values
+from torch import where
 
 from ...nodes.BufferNode import BufferNode
 from ...nodes.NodeException import NodeException
-from ...services.db.SQLService import SQLService
 from ...agents.Agent import Agent
 from ...nodes.Action import Action
-from ...nodes.ServiceNode import ServiceNode
 
 
 @dataclass
-class SQLAction(ServiceNode, BufferNode, Action):
-    """`Action` node to perform SQL operations using the referenced `SQLService`.
+class SQLAction(BufferNode, Action):
+    """`Action` node to perform SQL operations using the pyodbc library.
     """
     
-    query : str = field()
+    connection_str : str = field(default=None, metadata={"description": "connection string for the specific SQL database"})    
+    query : str = field(default=None, metadata={"description": "SQL query to execute. If `input_keys` are defined, the query is treated as a parameterized query and values are taken from the buffers."})
+    
+    def __post_init__(self):
+        super().__post_init__()
+        self._conn = None
+        self._cursor = None
     
     def _on_install(self, agent : Agent = None):
-        ServiceNode._on_install(self, agent)
-        BufferNode._on_install(self, agent)
-        if not isinstance(self._service, SQLService):
-            raise NodeException("referenced service is not an instance of " + SQLService.cname())
+        super()._on_install(agent)
+        self._conn = pyodbc.connect(self.connection_str)
+        self._cursor = self._conn.cursor()
         
-    
     def _on_execute(self):
-        """Executes the SQL query defined in the `query` attribute.
-            If `input_keys` are defined, the query is treated as a parameterized query and values are taken from the buffers.
-        """
-        # TODO
-        pass
+        data = self.get_parent_data()
+        # check for query type
+        if self.query.strip().lower().startswith("select"):
+            self._cursor.execute(self.query)
+        elif self.query.strip().lower().startswith("insert"):
+            columns = ', '.join(data.keys())
+            placeholders = ', '.join(['?'] * len(data))
+            query = self.query
+            if "(COLUMNS)" in query and "(VALUES)" in query:
+                query = query.replace("(COLUMNS)", f"({columns})")
+                query = query.replace("(VALUES)", f"({placeholders})")
+                self._cursor.execute(query, list(data.values()))
+            else:
+                self._cursor.execute(query)
+        elif self.query.strip().lower().startswith("update"):
+            if "(SET)" in self.query:
+                set_clause = ', '.join([f"{col} = ?" for col in data.keys()])            
+                query = self.query
+                query = query.replace("(SET)", set_clause)
+                self._cursor.execute(query, tuple(data.values()))
+            else:
+                self._cursor.execute(self.query)
+        elif self.query.strip().lower().startswith("delete"):
+            self._cursor.execute(self.query)
+        else:
+            self._cursor.execute(self.query)
+        
+        self._conn.commit()
+        results = self._cursor.fetchall()
+        columns = [column[0] for column in self._cursor.description]
+        data_with_columns = [dict(zip(columns, row)) for row in results]
+        self.add_data(data_with_columns)
---
+++ tests/unit/nodes/db/test_sql_action.py
@@ -0,0 +1,4 @@
+from pydag.nodes.db.SQLAction import SQLAction
+
+def test_sql_select():
+    sa = SQLAction()
