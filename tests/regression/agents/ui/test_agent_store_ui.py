4937351483374f5193a34dc75accddc7626d429e
jhillenbrand <hillenbrandjonas@hotmail.com>
2026-07-06 00:54:58 +0200

    added config files and log test

--- tests/regression/agents/ui/test_agent_store_ui.py
+++ tests/regression/agents/ui/test_agent_store_ui.py
@@ -1,11 +1,25 @@
-
 import os
+from loguru import logger
 
 from pydag.agents.AgentStore import AgentStore
 from pydag.agents.auth.Auth import AuthManager
 
 
-def test_agent_store_open():
-    AuthManager(os.path.dirname(__file__) + "/users.yaml")
+def test_agent_store_open():    
+
+    # Remove default console handler (optional)
+    #logger.remove()
+
+    # Add file handler
+    logger.add(
+        os.path.dirname(__file__) + os.sep + "test.log",           # log file name
+        rotation="10 MB",    # rotate after file reaches 10 MB
+        retention="10 days", # keep logs for 10 days
+        level="INFO",        # minimum log level
+        format="{time} | {level} | {name}:{function}:{line} - {message}"
+    )
+    
+    AuthManager(os.path.dirname(__file__) + os.sep + "users.yaml")
+    
     ags : AgentStore = AgentStore(template_paths=[os.path.dirname(__file__)])
     ags.open()
